bdfb0621cd47d312dde18011054b941be0ceab03
lalyns <kelwho@naver.com>
2023-01-17 12:54:02 +0900

    #1 [Build] 운영자 권한 기능 Controller, Service 기초 구현
    Controller: Auth -> 운영자 Controller 추가
    Service: Auth -> 운영자 기능 추가

--- market/src/main/java/com/team2/market/controller/AuthController.java
+++ market/src/main/java/com/team2/market/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.team2.market.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.team2.market.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -19,4 +22,30 @@ public class AuthController {
         authService.requestAuthorization();
     }
 
+    @GetMapping("/seller")
+    public void getAllSellers() {
+        authService.getAllSellers();
+    }
+
+    @GetMapping("/user")
+    public void getAllUsers() {
+        authService.getCustomInfo();
+    }
+
+    @GetMapping("/auth/seller") 
+    public void getAllAuthRequest() {
+
+    }
+
+    // Post나 Put 메소드 뭘 써야할지 잘몰라서 일시적으로 Post로 작성
+    @PostMapping("/auth/seller/{userid}")
+    public void changeAuth() {
+        authService.setAuthorization();
+    }
+
+    @GetMapping("/auth/seller/{sellerid}")
+    public void getMethodName() {
+        authService.getSellerInfo();
+    }
+    
 }
---
+++ market/src/main/java/com/team2/market/dto/product/response/PostOrderResponseDto.java
@@ -0,0 +1,5 @@
+package com.team2.market.controller;
+
+public class PostOrderResponseDto {
+
+}
