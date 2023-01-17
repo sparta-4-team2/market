595d5c87692c49d4c02f508135b268acfe5a2bdd
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-17 19:34:17 +0900

    #6 [feat]  판매글 작성 구현
    PostCreateRequestDto ->  request값 가져오기
    PostCreateResponseDto ->  response값 불러오기

--- market/src/main/java/com/team2/market/dto/product/response/PostCreateResponseDto.java
+++ market/src/main/java/com/team2/market/dto/product/response/PostCreateResponseDto.java
@@ -1,5 +1,24 @@
 package com.team2.market.dto.product.response;
 
-public class PostCreateResponseDto {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor//lombok 사용해서 필요는 X
+public class PostCreateResponseDto {
+    //등록된 상품 name, price, contents, productid
+    private String title;
+    private String productName;
+    private int price;
+    private long product_id; //post에는 없음(erd에만...)
+    private String contents;
+    public PostCreateResponseDto(String title, String productName, int price, long product_id, String contents) {
+        this.title = title;
+        this.productName = productName;
+        this.price = price;
+        this.product_id = product_id;
+        this.contents = contents;
+    }
 }
