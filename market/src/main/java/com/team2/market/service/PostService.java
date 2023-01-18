d3a2177c1d8d6db84ad5b1b2046611250da8ccf9
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-18 11:26:56 +0900

    #6 [feat] create 기능,  jwt 토큰 기능, 상품 게시글 등록 기능
    Post >  Entity ->Dto
    PostService > jwt 토큰 기능, DB 조회 후 저장할 객체 만들기,

--- market/src/main/java/com/team2/market/service/PostService.java
+++ market/src/main/java/com/team2/market/service/PostService.java
@@ -3,12 +3,12 @@ package com.team2.market.service;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
 import com.team2.market.dto.product.request.*;
 import com.team2.market.dto.product.response.*;
-import com.team2.market.entity.Order;
 import com.team2.market.entity.Post;
 import com.team2.market.entity.User;
 import com.team2.market.repository.OrderRepository;
@@ -28,14 +28,37 @@ public class PostService implements PostServiceInterface{
     private final OrderRepository orderRepository;
     private final JwtUtil jwtUtil;
 
-    @Override
+    @Transactional//상품 게시글 등록
     public PostCreateResponseDto createPost(PostCreateRequestDto requestDto, HttpServletRequest request) {
-        // TODO Auto-generated method stub
-        return null;
+        // Request에서 Token 가져오기
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+
+        // 토큰이 있는 경우에만 관심상품 추가 가능
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                // 토큰에서 사용자 정보 가져오기
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("Token Error");
+            }
+
+            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
+            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+            );
+
+            // 요청받은 DTO 로 DB에 저장할 객체 만들기
+            Post post = postRepository.saveAndFlush(new Post(requestDto, user.getId()));
+
+            return new PostCreateResponseDto(post);
+        } else {
+            return null;
+        }
     }
 
     @Override
-    public PostGetResponseDto getPost(PostGetRequestDto requestDto, Long postid, HttpServletRequest request) {
+    public List<PostGetRequestDto> getPost(PostGetRequestDto requestDto, Long postid, HttpServletRequest request) {
         // TODO Auto-generated method stub
         return null;
     }
