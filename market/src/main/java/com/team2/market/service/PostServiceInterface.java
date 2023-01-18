afcb158a555da5b919c4850a84e902f87dc9458b
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-18 19:34:16 +0900

    #7 [feature]  관심 상품 조회, 전체 상품 조회 구현
    PostService : getPost에서 jwt토큰 사용 db조회
    PostController : List<PostGetRequestDto>, List<Post> 형태로 각각 관심상품 조회, 전체상품 조회
    PostServiceinterface : List<PostGetRequestDto> getPost, List<Post> getAllPost 형태로 주입

--- market/src/main/java/com/team2/market/service/PostServiceInterface.java
+++ market/src/main/java/com/team2/market/service/PostServiceInterface.java
@@ -6,11 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.team2.market.dto.product.request.*;
 import com.team2.market.dto.product.response.*;
+import com.team2.market.entity.Post;
 
 public interface PostServiceInterface {
     PostCreateResponseDto createPost(PostCreateRequestDto requestDto, HttpServletRequest request);
     List<PostGetRequestDto> getPost(PostGetRequestDto requestDto, Long postid, HttpServletRequest request);
-    List<PostGetResponseDto> getAllPost(PostGetRequestDto requestDto, HttpServletRequest request);
+    List<Post> getAllPost(PostGetRequestDto requestDto, HttpServletRequest request);
     PostUpdateResponseDto updatePost(PostUpdateRequestDto requestDto, Long postid, HttpServletRequest request);
     PostDeleteResponseDto deletePost(PostDeleteRequestDto requestDto, Long postid, HttpServletRequest request);
 }
