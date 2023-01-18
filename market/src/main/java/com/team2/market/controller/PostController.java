90e547c793f0ac41fc66074fdf198b1317ba5beb
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-18 19:34:52 +0900

    Revert "#7 [feature]  관심 상품 조회, 전체 상품 조회 구현"
    This reverts commit afcb158a555da5b919c4850a84e902f87dc9458b.

--- market/src/main/java/com/team2/market/controller/PostController.java
+++ market/src/main/java/com/team2/market/controller/PostController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.team2.market.entity.Post;
 import org.springframework.web.bind.annotation.*;
 
 import com.team2.market.dto.product.request.*;
@@ -42,8 +41,8 @@ public class PostController {
 
     
     @GetMapping("/posts")
-    public List<Post> getAllPost(@RequestBody PostGetRequestDto requestDto,
-                                 HttpServletRequest request)
+    public List<PostGetResponseDto> getAllPost(@RequestBody PostGetRequestDto requestDto,
+                                               HttpServletRequest request)
     {
         return productService.getAllPost(requestDto, request);
     }
